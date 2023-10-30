4d8b15bced4bd1e35995923bacfd97515a6736df
NeugeMa <mariana.n.dourado@gmail.com>
2023-10-30 17:29:01 -0300

    criação pagina vistoria + removendo conteudo da home

---
+++ src/app/vistoria/page.jsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Vistoria() {
+    return (
+        <> 
+            <h1> Vistoria da Boa! </h1>
+            <Link href="/"> Voltar </Link>
+        </>
+    )
+}
