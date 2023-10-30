127991f3aadd893f897a10ce7dd63cd63deffdba
NeugeMa <mariana.n.dourado@gmail.com>
2023-10-30 17:42:42 -0300

    area cliente tailwind feito!!

--- src/components/Cabecalho/cabecalho.jsx
+++ src/components/Cabecalho/cabecalho.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import './cabecalho.css'; 
+// import './cabecalho.css'; 
 import Link from "next/link";
 
 
@@ -7,8 +7,17 @@ export default function Cabecalho() {
     return (
         <> 
             <header className="bg-grey-300 h-18">     
-                <h1> Área Cliente </h1> 
-                <nav className="flex justify-around">
+            
+                <div className="flex flex-wrap items-center rounded-b-xl border-t-2"> 
+                    <button className="border-2 border-blue-600 rounded-2xl text-blue-600 shadow-sm p-1 px-2 m-2">
+                        Área Cliente
+                    </button>
+                    
+                </div> 
+
+
+
+                <nav className="flex justify-around bg-neutral-300">
                     <Link className="cabecalho-links" href="/contratacao"> Constratação</Link>
                     <Link className="cabecalho-links" href="/vistoria"> Vistoria</Link>
                     <Link className="cabecalho-links" href="/vistoria"> Ajuda</Link>
