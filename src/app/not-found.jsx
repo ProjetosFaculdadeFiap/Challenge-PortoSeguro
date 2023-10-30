4216fd81b693d3f13ce372706705599e694169d7
NeugeMa <mariana.n.dourado@gmail.com>
2023-10-30 17:28:37 -0300

    criação página Erro404

---
+++ src/app/not-found.jsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function NotFound() {
+    return (
+        <> 
+            <h1 className="flex justify-center"> <strong> OOPS! 404  </strong></h1> 
+                <p className="flex justify-center"> parece que você está tentando acessar uma página que não existe! </p> 
+                <p className="flex justify-center"> <Link href="/"> Volte a página <strong>Home</strong> antes que você se perca!</Link></p>
+        </>
+    )
+}
