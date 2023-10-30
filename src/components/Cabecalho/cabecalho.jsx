58d9d0d85bfc256b13d868ff22142aeb6769c7d1
NeugeMa <mariana.n.dourado@gmail.com>
2023-10-30 18:20:10 -0300

    finalizando cabecalho + layout

--- src/components/Cabecalho/cabecalho.jsx
+++ src/components/Cabecalho/cabecalho.jsx
@@ -1,31 +1,38 @@
 import React from "react";
-// import './cabecalho.css'; 
 import Link from "next/link";
-
+import Image from "next/image";
 
 export default function Cabecalho() {
     return (
-        <> 
-            <header className="bg-grey-300 h-18">     
-            
-                <div className="flex flex-wrap items-center rounded-b-xl border-t-2"> 
-                    <button className="border-2 border-blue-600 rounded-2xl text-blue-600 shadow-sm p-1 px-2 m-2">
-                        Área Cliente
-                    </button>
-                    
-                </div> 
-
-
+        <>
+            <header>
+                <div className="flex items-center justify-between rounded-b-xl border-t-2">
+                    <div>
+                        <a href="https://www.portoseguro.com.br/pdc/login">
+                            <button className="border-2 border-blue-600 rounded-2xl text-blue-600 shadow-sm p-1 px-2 m-2">
+                                Área Cliente
+                            </button>
+                        </a>
+                    </div>
+                    <div className="flex items-center">
+                        <Image
+                            src="/img/logo-porto01.png"
+                            alt="Logo Porto"
+                            width={200}
+                            height={100}
+                        />
+                    </div>
+                </div>
 
                 <nav className="flex justify-around bg-neutral-300">
-                    <Link className="cabecalho-links" href="/contratacao"> Constratação</Link>
+                    <Link className="cabecalho-links" href="https://www.portoseguro.com.br/"> Contratação</Link>
                     <Link className="cabecalho-links" href="/vistoria"> Vistoria</Link>
-                    <Link className="cabecalho-links" href="/vistoria"> Ajuda</Link>
-                    <Link className="cabecalho-links" href="/vistoria"> Sobre Nós</Link>
+                    <Link className="cabecalho-links" href="https://www.portoseguro.com.br/canal-de-ajuda"> Ajuda</Link>
+                    <Link className="cabecalho-links" href="https://www.portoseguro.com.br/institucional/sobre-a-porto-seguro"> Sobre Nós</Link>
                 </nav>
             </header>
         </>
     );
-};
+}
 
 
