3bb36453db50bad30c65c666090f8d31cfb33251
NeugeMa <mariana.n.dourado@gmail.com>
2023-10-31 08:49:17 -0300

    página "index" (FEITA) está localizada a nossa descrição de projeto | falta apenas a estilização

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,7 +2,23 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <> 
+    <>
+      <h1> Projeto: Challenge Porto Seguro - Bikes </h1> 
+
+      <h2> Descrição da Solução Proposta </h2>
+      <p> Queremos propor a ideia de uma IA (inteligência artificial) que possa ajudar na analise de fotos de bikes, dispensando a necessidade de uma pessoa.
+        <br /> Além disso, há outros pontos que serão alterados e melhorados com essa automação, sendo eles: </p>
+
+      <br />
+      <p> 
+      Cliente e ChatBot <br />
+      Redução da quantidade de fotos; <br />
+      Problema com a qualidade das fotos; <br />
+      Auxílio ao cliente no momento da fotografia;  <br />
+      Construção de uma melhora de interface; <br />
+      Criação da vistoria completamente digital;  <br />
+      Evitar fraudes;
+      </p>
     </> 
   )
 }
