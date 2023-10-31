7f57f856169d9e9e997caa5a3a7c5158864bba94
NeugeMa <mariana.n.dourado@gmail.com>
2023-10-31 08:48:48 -0300

    página sobre nós (FEITA) | falta apenas a estilização

---
+++ src/app/sobre/page.jsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+export default function SobreNos() {
+    return (
+        <> 
+            <h1> Lista de Membros - Tropinha do Challenge </h1>
+
+            <br />
+            <p> 
+            Felipe Moraes Silva - RM 98455 <br />
+            Gabriel Ribeiro Costa - RM 551727 <br />
+            Gustavo Moreira Golçalves - RM 97999 <br />
+            Mariana Neugebauer Dourado - RM 550494 <br />
+            Vinícius de Alencar Chagas - RM 550167 <br />
+            </p>
+
+            <br />
+            <Link href="/">Volte para a <strong> Home </strong></Link>
+        </>
+    )
+}
