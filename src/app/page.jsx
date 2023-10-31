dead8a7e2526c677f3eadb5ddd1f9eb3eb9af25d
NeugeMa <mariana.n.dourado@gmail.com>
2023-10-31 11:49:28 -0300

    finalizando a página principal + utilizando o global.css para localizarmos a estilização :)

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,24 +1,26 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
     <>
-      <h1> Projeto: Challenge Porto Seguro - Bikes </h1> 
+      <h1 className="flex justify-center"> <strong> Projeto: Challenge Porto Seguro - Bikes </strong></h1> 
+        <h2 className="flex justify-center"> <strong> Descrição da Solução Proposta</strong></h2> <br />
+          <p className="flex justify-center"> Queremos propor a ideia de uma IA (inteligência artificial) que possa ajudar na analise de fotos de bikes, dispensando a necessidade de uma pessoa. <br /> </p>
+            <p className="flex justify-center"> Além disso, há outros pontos que serão alterados e melhorados com essa automação, sendo eles:</p>
+              <br />
 
-      <h2> Descrição da Solução Proposta </h2>
-      <p> Queremos propor a ideia de uma IA (inteligência artificial) que possa ajudar na analise de fotos de bikes, dispensando a necessidade de uma pessoa.
-        <br /> Além disso, há outros pontos que serão alterados e melhorados com essa automação, sendo eles: </p>
+          <div className="p-1 px-5">
+            <div class="card1">
+                    <ol class="lista-propostas">
+                        <li class="item">Cliente e ChatBot</li>
+                        <li class="item">Redução da quantidade de fotos; </li>
+                        <li class="item">Problema com a qualidade das fotos;</li>
+                        <li class="item">Auxílio ao cliente no momento da fotografia;</li>
+                        <li class="item">Construção de uma melhora de interface;</li>
+                        <li class="item">Criação da vistoria completamente digital;</li>
+                        <li class="item">Evitar fraudes;</li>
+                    </ol>
+                </div>
+            </div>
 
-      <br />
-      <p> 
-      Cliente e ChatBot <br />
-      Redução da quantidade de fotos; <br />
-      Problema com a qualidade das fotos; <br />
-      Auxílio ao cliente no momento da fotografia;  <br />
-      Construção de uma melhora de interface; <br />
-      Criação da vistoria completamente digital;  <br />
-      Evitar fraudes;
-      </p>
     </> 
   )
 }
