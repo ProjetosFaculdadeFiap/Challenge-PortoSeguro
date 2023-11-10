222c6d7d7ea77130700144f845ad0acc8c351e0b
NeugeMa <mariana.n.dourado@gmail.com>
2023-11-10 11:38:21 -0300

    refazendo página de login - utilizando método post

--- src/app/cadastro/login/page.jsx
+++ src/app/cadastro/login/page.jsx
@@ -2,6 +2,8 @@
 import { useState } from 'react';
 import './login.css'
 
+export default function Login() { 
+
 // Armazenando o objeto "usuário" que deve ser preenchido no formulário, e enviado na base de dados
 const [usuario, setUsuario] = useState ({
     info: "login", 
@@ -21,29 +23,52 @@ const handleSubmit = async (e) => {
     // Requisição utilizando POST 
     // Requisição será enviada para a rota: /api/login
     // Requisição será enviada com os dados do formulário 
-    const resposta = await fetch("http://localhost:3001/dados/base/base-users", {
+    const resposta = await fetch("http://localhost:3000/dados/base/base-users", {
         method: "POST", 
         body: JSON.stringfy(usuario), }); 
 
+    // Armazenando resultado em formarto JSON 
+    const resultado = await resposta.json();
 
-
-
-}
-
-
-
-export default function Login () {
+            //Verificando resposta
+            if (resultado.status) {
+                //Redirecionar para a página de produtos.
+                window.location.href = "/";
+            } else {
+                //Exibir uma mensagem de erro.
+                alert("Usuário ou senha inválidos!");
+            }  };
     return (
         <> 
             <div className="form-container"> 
-                <form id="loginForm" class="form">
-                        <h2>Acesse sua Conta</h2>
-                        <input type="text" placeholder="Digite seu Email: " required/>
-                        <input type="password" placeholder="Digite sua Senha: " required/>
-                        <button className="botao-login" type="submit">Login</button>
-                </form>
+                <h2>Acesse sua Conta</h2>
+                    <form id="loginForm" class="form" onSubmit={handleSubmit}>
+                        <fieldset>
+
+                            <div>
+                            <label htmlFor="idEmail">Digite seu email:</label>
+                            <input
+                                type="email"
+                                name="email"
+                                id="idEmail"
+                                placeholder="Digite seu e-mail" value={usuario.email} onChange={handleChange} />
+                            </div>
+
+                            <div>
+                            <label htmlFor="idSenha">Digite sua senha:</label>
+                            <input
+                                type="password"
+                                name="senha"
+                                id="idSenha"
+                                placeholder="Digite sua senha" value={usuario.senha} onChange={handleChange} />
+                            </div>
+
+                            <div>
+                                <button className="botao-login" type="submit">Entrar</button>
+                            </div>
+                    </fieldset>
+                </form> 
             </div>
         </>
     )
 }
-
