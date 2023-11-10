03bd8a582dbfea4a093e46dfd67122098352c1ff
NeugeMa <mariana.n.dourado@gmail.com>
2023-11-10 11:02:34 -0300

    Add login and cadastro functionality

--- src/app/cadastro/login/page.jsx
+++ src/app/cadastro/login/page.jsx
@@ -1,4 +1,36 @@
+"use client"; 
+import { useState } from 'react';
 import './login.css'
+
+// Armazenando o objeto "usuário" que deve ser preenchido no formulário, e enviado na base de dados
+const [usuario, setUsuario] = useState ({
+    info: "login", 
+    email: "", 
+    senha: "", 
+})
+
+// Função de capturar dados
+const handleChange = (e) => {
+    const { name, value } = e.target; 
+    setUsuario({...usuario, [name]: value}); 
+}
+
+const handleSubmit = async (e) => {
+    e.preventDefault(); 
+
+    // Requisição utilizando POST 
+    // Requisição será enviada para a rota: /api/login
+    // Requisição será enviada com os dados do formulário 
+    const resposta = await fetch("http://localhost:3001/dados/base/base-users", {
+        method: "POST", 
+        body: JSON.stringfy(usuario), }); 
+
+
+
+
+}
+
+
 
 export default function Login () {
     return (
@@ -6,7 +38,7 @@ export default function Login () {
             <div className="form-container"> 
                 <form id="loginForm" class="form">
                         <h2>Acesse sua Conta</h2>
-                        <input type="text" placeholder="Digite seu Nome ou Email: " required/>
+                        <input type="text" placeholder="Digite seu Email: " required/>
                         <input type="password" placeholder="Digite sua Senha: " required/>
                         <button className="botao-login" type="submit">Login</button>
                 </form>
@@ -14,3 +46,4 @@ export default function Login () {
         </>
     )
 }
+
--- src/app/cadastro/page.jsx
+++ src/app/cadastro/page.jsx
@@ -1,25 +1,57 @@
+"use client"; 
 import './cadastro.css'
+import React, { useState } from "react";
 
-export default function Cadastro() {
+const Cadastro = () => {
+    const [nome, setNome] = useState("");
+    const [email, setEmail] = useState("");
+    const [senha, setSenha] = useState("");
+    const [mensagem, setMensagem] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setMensagem("Cadastro realizado com sucesso!");
+    };
+
     return (
         <>
-            <div class="form-container">
-                    <form id="createAccountForm" class="form">
-                        <h2>Crie sua conta :D</h2>
-                        <input type="text" placeholder="Digite seu Nome:" required/> 
-                        <input type="email" placeholder="Digite seu Email:" required/>
-                        <input type="password" placeholder="Crie uma Senha:" required/>
-                        <button className="botao-cadastro" type="submit">Cadastrar</button>
-                    </form>
+            <div className="form-container">
+                <form className="form" onSubmit={handleSubmit}>
+                    <h2>Crie sua conta :D</h2>
+                    <input
+                        type="text"
+                        placeholder="Digite seu Nome:"
+                        required
+                        value={nome}
+                        onChange={(event) => setNome(event.target.value)}
+                    />
+                    <input
+                        type="email"
+                        placeholder="Digite seu Email:"
+                        required
+                        value={email}
+                        onChange={(event) => setEmail(event.target.value)}
+                    />
+                    <input
+                        type="senha"
+                        placeholder="Crie uma Senha:"
+                        required
+                        value={senha}
+                        onChange={(event) => setSenha(event.target.value)}
+                    />
+                    <p>{mensagem}</p>
+                    <button className="botao-cadastro" type="submit">
+                        Cadastrar
+                    </button>
+                </form>
 
-            <div> 
-                <a href="/cadastro/login">
-                    <button className="botao-login">
-                            Acesse sua Conta :D 
-                        </button>
+                <div>
+                    <a href="/cadastro/login">
+                        <button className="botao-login">Acesse sua Conta :D</button>
                     </a>
                 </div>
-                </div>
+            </div>
         </>
-    )
-}
+    );
+};  
+export default Cadastro;
