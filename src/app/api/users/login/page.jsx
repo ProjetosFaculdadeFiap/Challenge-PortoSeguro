5e2088bc0a908ab63c9f953fd4f2d4c91a9ff62b
felipemoraes <rm98455@fiap.com.br>
2023-11-13 11:28:05 -0300

    page.jsx em login finalizado

---
+++ src/app/api/users/login/page.jsx
@@ -0,0 +1,21 @@
+import Link from 'next/link'
+import './login.css'
+export default function LoginPage() {
+return (
+    <>
+      <div className="form-container">
+          <form className="form">
+          <h2>Acesse sua Conta</h2>
+              <input type="email" placeholder='Seu e-mail:'/>
+              <input type="password" placeholder='Sua senha:'/>
+
+
+              <div>
+                <button className='botao-login' type="submit"> Entrar </button>
+                <Link href='/api/users/cadastro'> Ainda não possui conta?</Link>
+              </div>
+            </form>
+      </div>
+    </>
+  )
+}
