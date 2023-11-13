1ce2f75ce8c77c89187c461b749126a00212e1d7
NeugeMa <mariana.n.dourado@gmail.com>
2023-11-13 09:12:38 -0300

    refazendo página de login :)

--- src/app/usuarios-porto/login/page.jsx
+++ src/app/usuarios-porto/login/page.jsx
@@ -1,21 +1,73 @@
+"use client";
 import Link from 'next/link'
+import { useState } from "react";
 import './login.css'
 
-export default function LoginPage() {
+
+export default function Login() {
+  const [usuario, setUsuario] = useState({
+    info: "login",
+    email: "",
+    senha: "",
+  });
+
+  const handleChange = (evt) => {
+    const { name, value } = evt.target;
+
+    setUsuario({ ...usuario, [name]: value });
+  };
+
+  const handleSubmit = async (evt) => {
+    evt.preventDefault();
+
+    const response = await fetch("http://localhost:3000/dados/base/base-users",
+      {
+        method: "POST",
+        body: JSON.stringify(usuario),
+      }
+    );
+
+    const resultado = await response.json();
+
+    if (resultado.status) {
+      window.location.href = "/";
+    } else {
+      alert("Perfil inválido! Você já tem cadastro?");
+    }
+  };
     return (
         <>
-            <div className="form-container"> 
-                    <form className="form"> 
-                    <h2>Acesse sua Conta</h2>
-                        <input type="email" placeholder='Seu e-mail:' /> 
-                        <input type="password" placeholder='Sua senha:'/>
+        <div className="form-container"> 
+            <form className="form"> 
+                <fieldset>
+                    <legend className='font-bold'>Acessando sua conta</legend>
+                    <div className="mb-4"> 
+                        <input
+                        className="w-full border border-2 rounded-md py-2 px-3"
+                        type="email" name="email" id="idEmail"
+                        placeholder="Digite seu e-mail:"
+                        value={usuario.email}
+                        onChange={handleChange} />
+                    </div>
+    
+                    <div className="mb-4">
+                        <input className="w-full border border-2 rounded-md py-2 px-3"
+                        type="password" name="senha" id="idSenha"
+                        placeholder="Digite sua senha:"
+                        value={usuario.senha}
+                        onChange={handleChange} />
+                    </div>
 
+                    <div className="text-center">
+                      <button className='botao-login' type="submit"> Entrar </button>
+                    </div>
+                </fieldset>
 
-                        <div> 
-                            <button className='botao-login' type="submit"> Entrar </button>
-                            <Link href='/usuarios-porto/cadastro'> Ainda não possui conta?</Link>
-                        </div> 
-                    </form>
+                    <div className="mt-4 text-center">
+                        <span>Ainda não possui um cadastro?</span><Link href="/usuarios-porto/cadastro">
+                        <p className="text-cyan-500">Clique aqui</p> </Link>
+                    </div>
+            </form>
             </div>
         </>
     )
